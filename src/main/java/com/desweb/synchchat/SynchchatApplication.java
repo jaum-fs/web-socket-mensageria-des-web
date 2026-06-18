d8c582199529adaa5a8b7fe4346417f82246fb20
Joao Marcos <martins_joao@id.uff.br>
2026-06-18 18:52:39 -0300

    criando projeto spring

---
+++ src/main/java/com/desweb/synchchat/SynchchatApplication.java
@@ -0,0 +1,13 @@
+package com.desweb.synchchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynchchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynchchatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desweb/synchchat/SynchchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desweb.synchchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynchchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
